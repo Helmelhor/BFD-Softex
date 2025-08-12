2a64b9fd1211a79b89e4bfb32390d619cf865389
Helmer Barcelos <hfelixmsouza@gmail.com>
2025-08-12 16:52:55 -0300

    up

---
+++ modulo_1/12.08.py
@@ -0,0 +1,68 @@
+#exercicio 1
+print("Questão 1")
+print("olá mundo")
+
+#exercicio 2
+print("Questão 2")
+print("1")
+print("2")
+print("3")
+
+#exercicio 3
+print("Questão 3")
+print("Nome: João\nIdade: 20 Anos\nCurso: Python Báscio")
+
+#exercicio 4
+print("Questão 4")
+nome = "helmer"
+idade = 19
+curso = "Python e Django"
+
+print(f"Oi meu nome é {nome}, tenho {idade} anos\ne atualmente estou cursando {curso} no BFD!")
+
+#exercicio 5
+print("Questão 5")
+print("####\n" "#  #\n" "#  #\n" "####")
+
+#parte 2, Operadores Matemáticos
+
+#exercicio 6
+print("Questão 6")
+a = 10
+b = 3
+
+print(f"Soma = {a+b}\nSubtração = {a-b}\nMultiplicação = {a*b}\nDivisão = {a/b}\nDivisão Inteira = {a//b}\nResto da divisão = {a%b}\nPotência = {a**b}")
+
+#exercicio 7
+print("Questão 7")
+
+#num1 = int(input("Digite um número: "))
+#num2 = int(input("Digite um número: "))
+
+#print(f"Soma = {num1+num2}\nSubtração = {num1-num2}\nMultiplicação = {num1*num2}\nDivisão = {num1/num2}")
+
+#exercicio 8
+print("Questão 8")
+
+#idade = int(input("digite sua idade: "))
+
+#dias_vividos = idade * 366
+
+#print(f"Você já viveu {float(dias_vividos)} dias")
+
+#exercicio 9
+print("Questão 9")
+
+#largura = float(input("Digite a largura do quadrado: "))
+#altura = float(input("Digite a altura do quadrado: "))
+
+#print(f"A área do retângulo é: {altura*largura} M²")
+
+#exercicio 10
+print("Questão 10")
+
+preco_produto = 20
+print(f"O preço do produto é {preco_produto}")
+valor_pago = float(input("Quanto você quer pagar? "))
+troco = preco_produto - valor_pago
+print(f"O troco será de: R${int(troco)}")
--- modulo_1/app1.py
+++ modulo_1/app1.py
@@ -1,1 +1,16 @@
+#dia 1 e 2
 print("hello world")
+
+#dia 3
+nome = "sein"
+idade = 20
+sexo = "m"
+
+print(type(idade))
+
+#__tipos de divisão__
+# / divisão normal, retorna float
+# // retorna numero inteiro
+# % resto da divisão
+
+
