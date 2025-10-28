4c68f68ad77ff4ad645ff81284b9acc3b3cc715c
Helmer Barcelos <126388456+Helmelhor@users.noreply.github.com>
2025-10-28 19:19:24 +0000

    atv sql 2

---
+++ modulo_3/atividade_sql_2.sql
@@ -0,0 +1,16 @@
+--1
+SELECT * from Aluno;
+select count(*) from Aluno;
+
+--2
+SELECT * from Curso;
+SELECT MIN(mensalidade) FROM Curso;
+
+--3
+SELECT max(nota1) from Aluno;
+
+--4
+SELECT SUM(mensalidade) from Curso;
+
+--5
+SELECT AVG(nota2) from Aluno;
